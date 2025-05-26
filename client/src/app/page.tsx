988e51720ad09a78296b704b2470a7689dd198db
Vaibhav Nayan <vaibhavron10@gmail.com>
2025-05-26 18:14:49 +0530

    completed Landing Page template

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import Navbar from "@/components/base/Navbar";
+import HeroSection from "@/components/base/HeroSection";
+import FeatureSection from "@/components/base/FeatureSection";
+import UserReviews from "@/components/base/UserReviews";
+import Footer from "@/components/base/Footer";
 
-export default function Home() {
+export default async function LandingPage() {
+  
   return (
-    <div>
-      Hello Bhai
+    <div className="min-h-screen flex flex-col ">
+      {/* Header */}
+      <Navbar />
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/base/Navbar.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+// import { CustomUser } from "@/app/api/auth/[...nextauth]/options";
+import { Button } from "../ui/button";
+// import LoginModal from "../auth/LoginModal";
+export default function Navbar({ user }: any) {
+  return (
+    <nav className="p-6 flex justify-between items-center bg-white shadow-sm">
+      <h1 className="text-xl md:text-2xl font-extrabold">QuickChat</h1>
+      <div className="flex items-center space-x-2 md:space-x-6 text-gray-700">
+        <Link href="/">Home</Link>
+        <Link href="#features">Features</Link>
+        <Link href="/dashboard">
+            <Button>Dashboard</Button>
+          </Link>
+        {/* {!user ? (
+          <LoginModal />
+        ) : (
+          
+        )} */}
+      </div>
+    </nav>
+  );
+}
