763e1947fb5fa78856b4a7cc7bb0175d28536602
Vaibhav Nayan <vaibhavron10@gmail.com>
2025-05-26 17:50:43 +0530

    Project Initialized

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+})
+export const metadata: Metadata = {
+  title: "Quick Chat App",
+  description: "To chat as quick as possible without login",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
