c48338a634d36fb2dd6b23e1459892c4d1bea657
Vaibhav Nayan <visualart013@gmail.com>
2025-06-17 06:32:12 +0530

    debugging

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
